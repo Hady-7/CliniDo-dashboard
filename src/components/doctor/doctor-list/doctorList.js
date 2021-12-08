d2a86852e1ba8c309bf8e642aeab8a0c5a47b6bb
Hady-7 <83098591+Hady-7@users.noreply.github.com>
2021-12-08 02:28:32 +0200

    afnan

--- src/components/doctor/doctor-list/doctorList.js
+++ src/components/doctor/doctor-list/doctorList.js
@@ -1,14 +1,19 @@
 import "./doctorList.css";
-
-
+import db from "../../../fbconifq/firebaseConfiq";
+import { onSnapshot, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 const DoctorList = () => {
- 
-  return (
-    <div>
-        doctor list
-    </div>
-  );
+  const [doctor, setDoctor] = useState([]);
+  useEffect(() => 
+    onSnapshot(collection(db, "Doctor"), (snapshot) => {
+      setDoctor(snapshot.docs.map((doc) => {
+        return { ...doc.data(), id: doc.id }
+      }))
+    })
+  ,[]);
+console.log(doctor);
+  return <div>doctor list</div>;
 };
 
 export default DoctorList;
